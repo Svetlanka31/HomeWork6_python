589ea041f986c7364740e86301a94764a9b81023
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-10-14 19:27:02 +0300

    added task0

---
+++ taask0.py
@@ -0,0 +1,10 @@
+# 1.Задайте список из нескольких чисел. 
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+import random
+
+list1 = [random.randint(1,10) for i in range(0,5)]
+print(list1)
+list2 = [list1[i] for i in range(1, len(list1), 2)]
+print(list2)
+print(sum(list2))
